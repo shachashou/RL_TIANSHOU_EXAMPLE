b3873703e119f901d805fba9f159487db9bfed0e
金南洋 <nanyang.jin@delixi-electric.com>
2025-06-11 16:22:25 +0800

    * 增加山地车

--- PLAY/mountaincar.py
+++ PLAY/mountaincar.py
@@ -4,13 +4,13 @@ import torch
 
 from POLICY.DQN import get_policy
 
-ENV_ID = 'CartPole-v1'
+ENV_ID = 'MountainCar-v0'
 
-POLICY_TRAIN_EPS = 0.1
+POLICY_TRAIN_EPS = 0.5  # 探索率
 POLICY_TEST_EPS = POLICY_TRAIN_EPS / 10
-POLICY_LR = 1e-3
-POLICY_DISCOUNT_FACTOR = 0.9
-POLICY_ESTIMATION_STEP = 3
+POLICY_LR = 1e-3  # 学习率
+POLICY_DISCOUNT_FACTOR = 0.99  # 远见程度， 越接近1越有远见
+POLICY_ESTIMATION_STEP = 3  # 往后看多步答案 ，简单任务就是3,迷宫的话，可能是10或者15
 POLICY_TARGET_UPDATE_FREQ = 320
 
 policy = get_policy(
